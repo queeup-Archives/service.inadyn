31564e91f83333678c6aa61147e8cc99d0f3a0be
queeup <ozgur.baskin@gmail.com>
2012-03-03 13:57:35 +0200

    reorganized service.py

--- service.py
+++ service.py
@@ -17,13 +17,12 @@ __language__ = __addon__.getLocalizedString
 __settings__ = __addon__.getSetting
 __set_settings__ = __addon__.setSetting
 
-STARTUP = True
 now = datetime.datetime.now()
 
 def check(process):
-  p1 = Popen(['ps', 'ax', '-o', 'pid,args'], shell=False, stdout=PIPE, stderr=PIPE)
-  p2 = Popen(['grep', process], shell=False, stdin=p1.stdout, stdout=PIPE, stderr=PIPE)
-  p3 = Popen(['grep', '-v', 'grep'], shell=False, stdin=p2.stdout, stdout=PIPE, stderr=PIPE)
+  p1 = Popen(['ps', 'ax', '-o', 'pid,args'], shell=False, stdout=PIPE, stderr=PIPE, close_fds=True)
+  p2 = Popen(['grep', process], shell=False, stdin=p1.stdout, stdout=PIPE, stderr=PIPE, close_fds=True)
+  p3 = Popen(['grep', '-v', 'grep'], shell=False, stdin=p2.stdout, stdout=PIPE, stderr=PIPE, close_fds=True)
   stdout, stderr = p3.communicate()
   if stdout:
     pid = re.findall('(\d+)', stdout)[0]
@@ -31,13 +30,12 @@ def check(process):
   return False, None
 
 def execute(arg):
-  p = Popen(arg, shell=False, stdout=PIPE)
+  p = Popen(arg, shell=False, stdout=PIPE, close_fds=True)
   pid = p.pid
   if pid:
     return True, pid + 1
 
 def kill(pid):
-  time.sleep(1)
   os.kill(int(pid), signal.SIGUSR1)
 
 def LOG(description):
@@ -51,13 +49,6 @@ def waiter(seconds, pid):
   LOG("Delaying %s secs" % seconds)
   for i in range(1, seconds):
     time.sleep(1)
-    # kill inadyn before xbmc
-    if xbmc.abortRequested == True:
-      #os.system('killall inadyn')
-      kill(pid)
-      if Debug: LOG('inadyn stoping!')
-      time.sleep(1)
-      sys.exit()
 
 # Get settings
 INADYN_START = __settings__('INADYN_START')
@@ -87,30 +78,37 @@ if INADYN_SYSTEM == 'dnsomatic@dyndns.org':
 else:
   inadyn += ['--dyndns_system', INADYN_SYSTEM]
 
-# check settings is allowing for service start with xbmc
-if INADYN_START == 'true':
-  while (not xbmc.abortRequested):
-    if STARTUP:
-      # make False for run service 1 time
-      STARTUP = False
-      _status, _pid = check('inadyn')
-      if not _status:
-        # if not executable change permission
-        if not os.access('%s/bin/inadyn' % __path__, os.X_OK):
-          os.chmod('%s/bin/inadyn' % __path__, 0755)
-        # Start service
-        status, pid = execute(inadyn)
-        if status:
-          if Debug: LOG('inadyn starting!')
-          #notification('inadyn service', 'Successfully started!')
-      else:
-        if Debug: LOG('inadyn already running!')
-        pid = _pid
-    if datetime.datetime.now().minute >= ((now.minute / 5) * 5) + 5 or (datetime.datetime.now().hour > now.hour) or (datetime.datetime.now().hour == 1 and now.hour == 12) or (datetime.datetime.now().hour == 0 and now.hour == 23) :
-      wait = 240
-    else:
-      wait = 3
-    waiter(wait, int(pid))
+# if not executable change permission
+if not os.access('%s/bin/inadyn' % __path__, os.X_OK):
+  os.chmod('%s/bin/inadyn' % __path__, 0755)
+
+# check if inadyn already running. If running find pid number 
+_status, _pid = check('inadyn')
+if not _status:
+  # check settings is allowing for service start with xbmc
+  if INADYN_START == 'true':
+    # Start service
+    status, pid = execute(inadyn)
+    if status:
+      if Debug: LOG('inadyn starting!')
+  else:
+    if Debug: LOG('inadyn service disabled from settings!!!')
+    sys.exit()
 else:
-  if Debug: LOG('inadyn service disabled from settings!!!')
+  if Debug: LOG('inadyn already running!')
+  pid = _pid
+
+
+while (not xbmc.abortRequested):
+  if datetime.datetime.now().minute >= ((now.minute / 5) * 5) + 5 or (datetime.datetime.now().hour > now.hour) or (datetime.datetime.now().hour == 1 and now.hour == 12) or (datetime.datetime.now().hour == 0 and now.hour == 23) :
+    wait = 240
+  else:
+    wait = 3
+  waiter(wait, int(pid))
+
+# kill inadyn before xbmc
+if xbmc.abortRequested == True:
+  kill(pid)
+  if Debug: LOG('inadyn stoping!')
+  time.sleep(1)
   sys.exit()
