a488511ee832d780c1d8f8a00feb2bf89a0bc1e5
queeup <ozgur.baskin@gmail.com>
2015-03-07 02:39:40 +0200

    cosmetic

--- service.py
+++ service.py
@@ -9,17 +9,22 @@ import xbmcaddon
 # DEBUG
 DEBUG = True
 
-__addon__ = xbmcaddon.Addon()
-__plugin__ = __addon__.getAddonInfo('name')
-__version__ = __addon__.getAddonInfo('version')
-__icon__ = __addon__.getAddonInfo('icon')
-__path__ = __addon__.getAddonInfo('path')
-__cachedir__ = __addon__.getAddonInfo('profile')
-__settings__ = __addon__.getSetting
+addon = xbmcaddon.Addon()
+addon_name = addon.getAddonInfo('name')
+addon_version = addon.getAddonInfo('version')
+addon_icon = addon.getAddonInfo('icon')
+addon_path = addon.getAddonInfo('path')
+addon_cachedir = addon.getAddonInfo('profile')
+addon_settings = addon.getSetting
 
 
 def log(description):
-  xbmc.log("[SERVICE] '%s v%s': DEBUG: %s" % (__plugin__, __version__, description.encode('ascii', 'ignore')), xbmc.LOGNOTICE)
+  xbmc.log("[DEBUG] '%s v%s': %s" % (addon_name, addon_version, description.encode('ascii', 'ignore')), xbmc.LOGNOTICE)
+
+
+def notification(title, message, image=addon_icon, displaytime=6000):
+    xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "GUI.ShowNotification", "params": {"title": "%s", "message": "%s", "image": "%s", "displaytime": %i}, "id": "%s"}' % \
+                        (title, message, image, displaytime, addon.getAddonInfo('id')))
 
 
 class Monitor(xbmc.Monitor):
@@ -34,46 +39,48 @@ class Monitor(xbmc.Monitor):
 
 class Main:
   def __init__(self):
-    self.pid = None
-    self._get_settings()
-    # self._monitor = Monitor(action=self.restart_service)
-    self._monitor = Monitor(restart=self.restart_service)
+    self._init_vars()
     # if not executable change permission
     if not os.access(self.INADYN_EXEC, os.X_OK):
       os.chmod(self.INADYN_EXEC, 0755)
     self._daemon()
 
+  def _init_vars(self):
+    self.pid = None
+    self._get_settings()
+    self._monitor = Monitor(restart=self.restart_service)
+     
   def _get_settings(self):
     # Get settings
-    self.INADYN_START = __settings__('INADYN_START')
-    self.INADYN_SYSTEM = __settings__('INADYN_SYSTEM')
-    self.INADYN_UPDATE = int(__settings__('INADYN_UPDATE')) * 60
-    self.INADYN_HOST = __settings__('INADYN_HOST')
-    self.INADYN_USER = __settings__('INADYN_USER')
-    self.INADYN_PWD = __settings__('INADYN_PWD')
-    self.INADYN_DBG = __settings__('INADYN_DBG')
+    self.INADYN_START = addon_settings('INADYN_START')
+    self.INADYN_SYSTEM = addon_settings('INADYN_SYSTEM')
+    self.INADYN_UPDATE = int(addon_settings('INADYN_UPDATE')) * 60
+    self.INADYN_HOST = addon_settings('INADYN_HOST')
+    self.INADYN_USER = addon_settings('INADYN_USER')
+    self.INADYN_PWD = addon_settings('INADYN_PWD')
+    self.INADYN_DBG = addon_settings('INADYN_DBG')
     
     # arm support
     if 'arm' in os.uname()[4]:
-      self.INADYN_EXEC = '%s/bin/inadyn.arm' % __path__
+      self.INADYN_EXEC = '%s/bin/inadyn.arm' % addon_path
     else:
-      # i386/i686/x86_64/arm binary support
-      self.INADYN_EXEC = '%s/bin/inadyn.%s' % (__path__, os.uname()[4])
-    self.INADYN_LOG = '%sinadyn.log' % xbmc.translatePath(__cachedir__)
-    self.INADYN_PID = '%sinadyn.pid' % xbmc.translatePath(__cachedir__)
+      # i386/i686/x86_64 binary support
+      self.INADYN_EXEC = '%s/bin/inadyn.%s' % (addon_path, os.uname()[4])
+    self.INADYN_LOG = '%sinadyn.log' % xbmc.translatePath(addon_cachedir)
+    self.INADYN_PID = '%sinadyn.pid' % xbmc.translatePath(addon_cachedir)
 
-    if int(__settings__('INADYN_SYSTEM_CONFIG')) == 1:
+    if int(addon_settings('INADYN_SYSTEM_CONFIG')) == 1:
       self.inadyn = [self.INADYN_EXEC,
                      '--period', str(self.INADYN_UPDATE),
                      '--system', 'custom@http_srv_basic_auth',
-                     '--server-name', __settings__('MANUAL_INADYN_SERVER_NAME'),
-                     '--server-url', __settings__('MANUAL_INADYN_SERVER_URL'),
+                     '--server-name', addon_settings('MANUAL_INADYN_SERVER_NAME'),
+                     '--server-url', addon_settings('MANUAL_INADYN_SERVER_URL'),
                      '--alias', self.INADYN_HOST,
                      '--username', self.INADYN_USER,
                      '--password', self.INADYN_PWD,
                      '--logfile', self.INADYN_LOG,
                      '--pidfile', self.INADYN_PID,
-                     '--cache-dir', xbmc.translatePath(__cachedir__),
+                     '--cache-dir', xbmc.translatePath(addon_cachedir),
                      '--verbose', self.INADYN_DBG,
                      '--background', ]
     else:
@@ -85,7 +92,7 @@ class Main:
                      '--password', self.INADYN_PWD,
                      '--logfile', self.INADYN_LOG,
                      '--pidfile', self.INADYN_PID,
-                     '--cache-dir', xbmc.translatePath(__cachedir__),
+                     '--cache-dir', xbmc.translatePath(addon_cachedir),
                      '--verbose', self.INADYN_DBG,
                      '--background', ]
 
@@ -119,10 +126,6 @@ class Main:
     except OSError:
       os.kill(int(pid) + 1, signal.SIGTERM)
 
-  def notification(self, title, message, image=__icon__, displaytime=6000):
-    xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "GUI.ShowNotification", "params": {"title": "%s", "message": "%s", "image": "%s", "displaytime": %i}, "id": "%s"}' % \
-                        (title, message, image, displaytime, __addon__.getAddonInfo('id')))
-
   def start_service(self):
     # check settings is allowing for service start with xbmc
     if self.INADYN_START == 'true':
@@ -133,24 +136,24 @@ class Main:
         status, self.pid = self.execute(self.inadyn)
         if status:
           if DEBUG:
-            log('inadyn starting!')
+            log('Service starting!')
       else:
         if DEBUG:
-          log('inadyn already running!')
+          log('Service already running!')
     else:
       if DEBUG:
-        log('inadyn service disabled from settings!!!')
-      # notification(__plugin__, 'inadyn service disabled from settings!!!')
+        log('Service disabled from settings!')
+      # notification(addon_name, 'inadyn service disabled from settings!!!')
 
   def stop_service(self):
     self.kill(self.pid)
 
   def restart_service(self):
-    self.kill(self.pid)
+    self.stop_service()
     # del self.pid because when restart, self.pid keeps old and wrong variable.
     del self.pid
     if DEBUG:
-      log('inadyn restarting!')
+      log('Service restarting!')
     # get new settings for inadyn execute
     self._get_settings()
     # start inadyn with new settings
@@ -160,10 +163,10 @@ class Main:
     self.start_service()
     while True:
       if self._monitor.waitForAbort(1):
-        log('Abort requested.')
+        log('Abort requested!')
         break
       xbmc.sleep(500)
-    log('Stoping Inadyn Service!')
+    log('Service stoping!')
     self.stop_service()
     del self._monitor
 
